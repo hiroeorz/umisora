7212a09ee38db5c1ba7b240262f238ceb2e83d9c
Shin Hiroe <hiroe.orz@gmail.com>
2017-05-30 08:37:54 +0900

    add navbar

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
 
 export interface Props {
-    content: string;
+    title: string;
+    value?: string;
 }
 
 export default class NavClass extends React.Component<Props, {}> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {value: props.value? props.value : ""};
+    }
+
     render() {
-        return <div>{this.props.content}</div>
+        return (
+          <div className="navbar">
+            <div className="navbar-inner">
+              <a className="brand" href="#">{this.props.title}</a>
+              <ul className="nav">
+                <li className="active"><a href="#">Link1</a></li>
+                <li><a href="#">Link2</a></li>
+              </ul>
+            </div>
+          </div>
+        )
     }
 }
